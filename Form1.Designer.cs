f1d21630a11bf2c9f09b1fab4ce622c77af6f1a9
Kent Aron Acebedo <moltenrockanivia@gmail.com>
2025-01-13 22:06:30 +0800

    First-Commmit
    01-13-25

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace VoiceCommand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.LimeGreen;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 611);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(532, 68);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Enter your text here...";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 685);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(111, 685);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Speak";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(156, 563);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 33);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Text to speech";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(403, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Command list";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LimeGreen;
+            this.pictureBox1.BackgroundImage = global::VoiceCommand.Properties.Resources.Untitled_design_2;
+            this.pictureBox1.Location = new System.Drawing.Point(111, 551);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(97, 225);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(345, 302);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-43, -224);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(599, 672);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(532, 731);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CODEX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;// using system
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Diagnostics;
+using System.Speech;
+
+
+
+namespace VoiceCommand
+{
+    public partial class Form1 : Form // eto for structure
+    {
+
+        #region Eto yung mga libraries na tinawag natin
+        // eto yung importat para ma access natin yung functionalities ng speech recognition
+        // eto yung mga naka declared sa taas System.Speech recognition, System.SpeechSynthesis
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer baste = new SpeechSynthesizer();
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+
+
+
+        Random rnd = new Random(); // for accesing the settings
+        int RecTime = 0; // this will help the system to run kung ano lang yung nasa list
+        DateTime TimeNow = DateTime.Now;
+        
+
+
+
+        #endregion
+
+        public Form1() //for structure
+        {
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            BackColor = Color.Transparent;
+            InitializeComponent();
+            baste.SpeakAsync("Welcome back user! I hope you are feeling good today!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //for structure method
+        {
+            _recognizer.SetInputToDefaultAudioDevice(); // eto yung responsible sa audio at microphone na gagamitin sa ating system
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+                                                                                         //nilagay ng methods
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+                                                                                        //nilagay sa methods
+            _recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(_recognizer_SpeechRecognized);
+           
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammar(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+
+                                                                                              //nilagay sa methods            
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startlistening_SpeechRecognized);
+          
+        }
+
+
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e) // for structure method
+        {
+            // this will be for our commands
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if (speech == "hello")
+            {
+                baste.SpeakAsync("How are you sir!");
+            }
+
+            if (speech == "motivate me codex")
+            {
+                baste.SpeakAsync(quotes());
+            }
+
+
+            if (speech == "stop talking")
+            {
+                baste.SpeakAsyncCancelAll();
+                ranNum = rnd.Next(1,2);    //ranNum is an integer
+
+                if (ranNum == 1)
+                {
+                    baste.SpeakAsync("Yes sir");
+                }
+
+                if (ranNum == 2)
+                {
+                    baste.SpeakAsync("Im sorry");
+                }
+            }
+
+            if (speech == "command list")
+            {
+                baste.Speak("here are my commands");
+                WindowState = FormWindowState.Normal;
+                Form cmd = new commands();
+                cmd.Show();
+
+            }
+            if (speech == "stop listening")
+            {
+                baste.SpeakAsync("If you need me just say, WAKE UP CODEX!");
+                _recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+
+            //switch statements
+
+            switch (speech)
+            {
+                //greetings
+                case("hi"):
+                    baste.SpeakAsync("Hello user");
+                    break;
+                case ("what is your name"):
+                    baste.SpeakAsync("good to see you my name is, Codex, I'm a voice assistant");
+                    break;
+                case ("hey"):
+                    baste.SpeakAsync("hey, what's up?");
+                    break;
+                case ("your history"):
+                    baste.SpeakAsync("My name is Codex. I was created by Sebastian's group for their final project in intermediate programming. I'm so blessed that they chose to make a voice command. " +
+                        "Such a wonderful journey for all of you guys. I will miss you all.");
+                    break;
+
+                //windows functionalities
+                case ("close"):
+                    baste.Speak("closing tab");
+                    SendKeys.Send("^{W}");
+                    break;
+                case ("exit"):
+                    baste.Speak("closing application");
+                    SendKeys.Send("%{F4}");                 
+                    break;
+                case ("bye"):
+                    baste.Speak("shutting down in, three, two, one, goodbye user, always remember that all is well");
+                    this.Close();
+                    break;
+                case ("hide"):
+                    baste.Speak("my bad. sorry sir");
+                    WindowState = FormWindowState.Minimized;
+                    break;
+
+                case ("show"):
+                    baste.Speak("I'm back!");
+                    WindowState = FormWindowState.Normal;
+                    break;
+
+                //time and date
+
+                case ("time today"):
+                    baste.Speak("The time today is" + DateTime.Now.ToShortTimeString());
+                    break;
+
+                case ("date today"):
+                    baste.Speak("Today is" + DateTime.Now.ToShortDateString());
+                    break;
+
+
+                //software and applications
+                case ("open word"):
+                    baste.Speak("Opening ms word");
+                    Process.Start("WINWORD.EXE");
+                    break;
+
+                case ("excel"):
+                    baste.Speak("opening excel");
+                    Process.Start("EXCEL.EXE");
+                    break;
+
+                case ("power point"):
+                    baste.Speak("opening power point");
+                    Process.Start("POWERPNT.EXE");
+                    break;
+                case ("paint"):
+                    baste.Speak("opening paint, practicing your art skill is very cool!");
+                    Process.Start("mspaint.exe");
+                    break;
+
+                //website
+
+                case ("facebook"):
+                    baste.Speak("opening facebook, always secure your account in any hacker");
+                    Process.Start("https://www.facebook.com/"); 
+                    break;
+                case ("gmail"):
+                    baste.Speak("always secure your email account");
+                    Process.Start("https://mail.google.com/");
+                    break;
+                case ("youtube"):
+                    baste.Speak("opening youtube, You can always save some time to relax yourself");
+                    Process.Start("https://www.youtube.com/");
+                    break;
+
+                //shortcut keys
+                case ("capture"):
+                    baste.Speak("capturing image");
+                    SendKeys.Send("{PRTSC}");
+                    break;
+
+                case ("paste"):
+                    baste.Speak("Pasting");
+                    SendKeys.Send("^{V}");
+                    break;
+
+                case ("select"):
+                    baste.Speak("okay sir");
+                    SendKeys.Send("{ENTER}");
+                    break;
+
+                case ("next"):
+                    SendKeys.Send("{RIGHT}");
+                    break;
+                case ("back"):
+                    SendKeys.Send("{LEFT}");
+                    break;
+
+                case ("go for"):
+                    baste.Speak("This?");
+                    SendKeys.Send("{TAB}");
+                    break;
+
+                case ("erase"):
+                    SendKeys.Send("{BKSP}");
+                    break;
+                case ("down"):
+                    SendKeys.Send("{DOWN}");
+                    break;
+                case ("up"):
+                    SendKeys.Send("{UP}");
+                    break;
+                case ("write"):
+                    string ch = speech;
+                    baste.Speak("typing");
+                    SendKeys.Send(ch.ToString());
+                    break;
+                case ("tab"):
+                    baste.Speak("choose tab");
+                    SendKeys.Send("%{TAB}");
+                    break;
+               
+            }
+
+        }
+
+
+
+        //All answer choices para di na kakaumay paulit ulit yung system
+
+        #region         
+           //structure
+        public string name() //this code is responsible if you ask the software's name
+        {
+            string[] greetings = new string[5] { "Hello my name is, Codex!  ", "ohh I am, Codex!", "Good to see you my name is, Codex!",
+                  "I am, Codex! ", "Good day! my name is, Codex! " };
+            Random r = new Random();
+            return greetings[r.Next(5)];
+        }
+
+        public string quotes() //this code is responsible if you ask the software's name
+        {
+            string[] greetings = new string[6] { "You can't win at everything but you can try", "Life isn't beautiful; it only becomes beautiful, once you start making it one",
+                "life doesn't give you what you want. Life gives you what you deserve","Many of life's failures are people who did not realize how close they were to success when they gave up.",
+                "The best way to predict your future is to create it.", "i'll leave tomorrows problems to tomorrows me" };
+            Random r = new Random();
+            return greetings[r.Next(6)];
+        }
+
+
+
+
+
+        #endregion
+
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTime = 0;
+        }
+
+
+
+     
+
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+            if (speech == "wake up codex")
+            {
+                startlistening.RecognizeAsyncCancel();
+                baste.SpeakAsync("ohhh, I'm back! What can I do for you?");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+
+
+
+        private void LstCommand_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (RecTime == 10) // responsible sa range ng time kung kailan lang mag accept ng mga commands ang software
+            {
+
+            }
+            else if (RecTime == 11) 
+            {
+                Timer.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTime = 0;
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer s = new SpeechSynthesizer();
+            string a;
+            a = richTextBox1.Text;
+            s.Speak(a);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+                this.Text = openFileDialog1.SafeFileName;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*WindowState = FormWindowState.Maximized;
+            Form cmd = new commands();
+            cmd.Show();*/
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+            Form cmd = new commands();
+            cmd.Show();
+        }
+    }
+}
